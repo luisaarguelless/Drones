4bc491ecec0674aa09abb9f2f8c13242433a59cc
luisarguelles <luisarguelles@seven4n.com>
2016-09-30 16:49:02 -0500

    Se termina clase que lee el archivo

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
+import io.Reader;
+import io.exception.ReaderException;
+
+import java.util.List;
+
 /**
  * Clase que tiene el metodo main()
  */
 public class Main {
     public static void main(String[] args){
-
+        Reader reader = new Reader("in.txt");
+        try{
+            List<String> lineas = reader.leerArchivo();
+            lineas.forEach(System.out::println);
+        }catch(ReaderException e){
+            e.printStackTrace();
+        }
     }
 }
+
